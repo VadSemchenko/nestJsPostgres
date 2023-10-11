9d7b75d8cf7ed63b3d559faf3951dbcd67f64004
Vadym Semcheno <vadym.semchenko@guesty.com>
2023-10-12 02:39:51 +0300

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { DefaultController } from './default/default.controller';
 import { DefaultService } from './default/default.service';
 import { WalletController } from './wallet/wallet.controller';
 import { WalletService } from './wallet/wallet.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '@a00190019AAa',
+      username: 'postgres',
+      // entities: [],
+      database: 'postgres',
+      synchronize: true,
+      // logging: true,
+    })
+  ],
   controllers: [DefaultController, WalletController],
   providers: [DefaultService, WalletService],
 })
