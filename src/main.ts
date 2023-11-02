63eb67d8bcded6f78e42438eb020e391f62e19e8
Vadym Semcheno <vadym.semchenko@guesty.com>
2023-11-03 01:17:56 +0200

    add additional models and dto

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { WalletModule } from './wallet/wallet.module';
+import { CreateWalletDto, FindByCompanyNameParams } from './wallet/wallet.dto';
+import { CreateTransactionDto } from './transaction/transaction.dto';
+import { TransactionModule } from './transaction/transaction.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  const options: SwaggerDocumentOptions = {
+    include: [WalletModule, TransactionModule],
+    extraModels: [CreateWalletDto, FindByCompanyNameParams, CreateTransactionDto]
+  };
   const config = new DocumentBuilder()
     .setTitle('Wallets')
     .setDescription('The wallets API description')
     .setVersion('0.0.1')
     .addTag('wallets')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
