de5d1570165046b6306d96ee066b8f78daab8260
Vadym Semcheno <vadym.semchenko@guesty.com>
2023-11-02 23:49:28 +0200

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Wallets')
+    .setDescription('The wallets API description')
+    .setVersion('0.0.1')
+    .addTag('wallets')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
